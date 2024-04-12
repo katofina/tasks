9471d8409db8aa0ec6ba80d56090e91f1ac6bdc0
katofina <askantya@tut.by>
2024-04-12 18:29:14 +0300

    resolve: add solutions, homeWork_03

--- app.js
+++ app.js
@@ -1,43 +1,60 @@
-const counter1 = {};
-const counter2 = new Object();
-const counter3 = Object.create({}, {p: {value:4, writable: true, enumerable:true, configurable: true}}); // можно указать прототип
-const obj = {p: 1,};
-const counter4 = Object.assign({}, obj);
+/*Массивы в JS являются "неправильными", потому что может хранить в себе данные разных
+типов(фиксится через TS) и является динамическим(позволяет изменять длину массива).
+Совмещает в себе такие структуры данных как стек, очередь, двустороннюю очередь и 
+упорядоченный список.*/
 
-const copyCounter1 = counter1; //копия ссылки
-const copyCounter2 = Object.assign({}, counter1); //поверхностное копирование
-const copyCounter3 = {...counter3}; //поверхностное копирование
-const copyCounter4 = JSON.parse(JSON.stringify(counter3)); //есть проблемы с undefined, function, symbol
-const copyCounter5 = structuredClone(counter4); //глубокое копирование
+function logger() {
+    console.log(`I output only external context: ${this.item}`);
+};
+const obj = {
+    item: "some value",
+};
+logger.bind(obj)();// Можно изначально привязать к переменной, чтобы в переменную вернулась новая функция, а затем вызвать через переменную
+logger.call(obj);
+logger.apply(obj);
 
-function makeCounter1() {};
-const makeCounter2 = function() {}; // Можно дать имя функции, можно присвоить стрелочную функцию.
-const makeCounter3 = new Function(); //передаётся строка
-(function() {})(); // Сразу вызывается.
+const arr1 = [1, 5, 6];
+const arr1Sum = arr1.reduce((acc, item) => acc += item, 0);
 
-/*Структурированное клонирование (метод structuredClone()) помогает создавать глубокие копии значений JS.
-Может работать с циклическими структурами данных, поддерживает множество встроенных типов данных, является
-более быстрым и надёжным. Однако отбрасывает цепочку прототипов объекта, поэтому если это экземпляр класса,
-то получим простой объект. Если объект содержит функции, то будут отброшены. Некоторые значения не
-структурированы для клонирования(Error, DOM-узлы) -> будет выброшено исключение. */
+const arr2 = ['Hello, ', 'World', '!'];
+const arr2Join = arr2.join('');
 
-const obj1 = { here: { is: "on", other: "3" }, object: "Y" };
-const obj2 = { here: { is: "on", other: "2" }, object: "Y" };
-const deepEqual = (obj1, obj2) => {
-    if (obj1.length !== obj2.length) return false;
-    if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 == null || obj2 == null) return false;
-    if (obj1 instanceof Array && obj2 instanceof Array) {
-        if (obj1.length !== obj2.length) return false;
-    };
-    for (let key in obj1) {
-        if (!obj2.hasOwnProperty(key)) return false;
-        if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-            if (!deepEqual(obj1[key], obj2[key])) return false;
-        } else if (obj1[key] !== obj2[key]) return false;
-    };
-    return true;
+const arr1Max = Math.max(...arr1);
+const arr1Min = Math.min(...arr1);
+
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    push(item) {
+        this.stack.push(item);
+    }
+    pop() {
+        return this.stack.pop();
+    }
+    get length() {
+        return this.stack.length;
+    }
 };
 
-function reverseStr(str) {
-    return str.split('').reverse().join('');
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    inqueue(item) {
+        this.queue.push(item);
+    }
+    delqueue() {
+        return this.queue.shift();
+    }
+    get length() {
+        return this.queue.length;
+    }
 };
+
+function likeBind(cntxt, ...args) {
+    const th = this;
+    return function() {
+        th.call(cntxt, args);
+    }; //Можно реализовать через apply th.apply(cntxt, []);
+}
